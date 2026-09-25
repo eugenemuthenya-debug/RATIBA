41cf8105091248e9e0081758427bae925dce361e
eugenemuthenya-debug <eugenemuthenya@gmail.com>
2026-09-26 01:32:13 +0300

    corrected database

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def signup():
         cursor.execute("""
             INSERT INTO users(
             username,
-            password_hash,
+            passwor_hash,
             email,
             phone_number)
             VALUES(%s,%s,%s,%s)
