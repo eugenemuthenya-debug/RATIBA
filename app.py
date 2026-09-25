9d656dc02889217900658432f32a8f8be7fb2797
eugenemuthenya-debug <eugenemuthenya@gmail.com>
2026-09-26 01:02:28 +0300

    changes1

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import   Flask , request , jsonify
 from flask_cors import CORS
 from flask_bcrypt import Bcrypt
+import psycopg
+import os
+import traceback
 
 app = Flask(__name__)
 CORS(app)
+
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+def get_db_connection():
+    return psycopg.connect(DATABASE_URL)
+
 
 @app.route("/api/sign-up", methods=["POST"])
 def signup():
@@ -19,6 +28,28 @@ def signup():
     if not all([username, password, email, phone_number]):
         return jsonify({"error": "All fields are required"}), 400
     hashed_password = Bcrypt.generate_password_hash(password).decode("utf-8")
+
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            INSERT INTO users(
+            username,
+            hashed_password,
+            email,
+            phone_number)
+            VALUES(%s,%s,%s,%s)
+            """,(username,
+                 hashed_password,
+                 email,
+                 phone_number))
+        
+        conn.commit()
+    except Exception as e:
+        traceback.print_exc()
+        return({"error":str(e)})
+
     return jsonify({"message":"Account created"}),200
 
 
